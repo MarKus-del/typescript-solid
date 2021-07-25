3172781301e2642f1244488e98f7bd88767cc4f8
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-07-25 11:58:40 -0300

    examplo de codigo sem principios

---
+++ src/srp/shopping-cart.ts
@@ -0,0 +1,69 @@
+type CartItem = { name: string; price: number };
+type OrderStatus = 'open' | 'closed';
+
+export class ShoppingCart {
+  private readonly _items: CartItem[] = [];
+  private _orderStatus: OrderStatus = 'open';
+
+  addItem(cartItem: CartItem): void {
+    this._items.push(cartItem);
+  }
+
+  removeItem(index: number): void {
+    this._items.splice(index, 1);
+  }
+
+  get items(): readonly CartItem[] {
+    return this._items;
+  }
+
+  get orderStatus(): OrderStatus {
+    return this._orderStatus;
+  }
+
+  total(): number {
+    return +this._items
+      .reduce((total, { price }) => (total += price), 0)
+      .toFixed(2);
+  }
+
+  checkout(): void {
+    if (this.isEmpty()) {
+      console.log('Seu carrinho está vazio');
+      return;
+    }
+
+    this._orderStatus = 'closed';
+    this.sendMessage('Seu pedido foi receido.');
+    this.saveOrder();
+    this.clear();
+  }
+
+  isEmpty() {
+    return this._items.length === 0;
+  }
+
+  sendMessage(msg: string): void {
+    console.log('Mensagem enviada: ' + msg);
+  }
+
+  saveOrder(): void {
+    console.log('Pedido salvo com sucesso');
+  }
+
+  clear(): void {
+    console.log('Carrinho de compras foi limpo...');
+
+    this._items.length === 0;
+  }
+}
+
+const shoppingCart = new ShoppingCart();
+shoppingCart.addItem({ name: 'Camiseta', price: 54.6 });
+shoppingCart.addItem({ name: 'Caderno', price: 12.5 });
+shoppingCart.addItem({ name: 'Lapis', price: 1.5 });
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+shoppingCart.checkout();
+console.log(shoppingCart.orderStatus);
